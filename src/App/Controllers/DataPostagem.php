52f123f17489d6bb541c575517d1eb0defe1792d
Cesar Augusto Sousa <cezzaar@gmail.com>
2016-09-06 10:04:04 -0300

    correcoes

--- src/App/Controllers/DataPostagem.php
+++ src/App/Controllers/DataPostagem.php
@@ -35,17 +35,17 @@ class DataPostagem
         $mensagem = 'postado agora';
         
         if ($dataAtual->format('Y') > $dataPostagem->format('Y')) {
-            $mensagem = $dataAtual->format('Y') - $dataPostagem->format('Y') . ' anos atras';
+            $mensagem = $y = $dataAtual->format('Y') - $dataPostagem->format('Y') . ' anos atrás';
         } elseif ($dataAtual->format('m') > $dataPostagem->format('m')) {
-            $mensagem = $dataAtual->format('m') - $dataPostagem->format('m') . ' meses atras';
+            $mensagem = $dataAtual->format('m') - $dataPostagem->format('m') . ' meses atrás';
         } elseif ($dataAtual->format('d') > $dataPostagem->format('d')) {
-            $mensagem = $dataAtual->format('d') - $dataPostagem->format('d') . ' dias atras';
+            $mensagem = $dataAtual->format('d') - $dataPostagem->format('d') . ' dias atrás';
         } elseif ($dataAtual->format('H') > $dataPostagem->format('H')) {
-            $mensagem = $dataAtual->format('H') - $dataPostagem->format('H') . ' horas atras';
+            $mensagem = $dataAtual->format('H') - $dataPostagem->format('H') . ' horas atrás';
         } elseif ($dataAtual->format('i') > $dataPostagem->format('m')) {
-            $mensagem = $dataAtual->format('i') - $dataPostagem->format('m') . ' minutos atras';
+            $mensagem = $dataAtual->format('i') - $dataPostagem->format('m') . ' minutos atrás';
         } elseif ($dataAtual->format('s') > $dataPostagem->format('s')) {
-            $mensagem = $dataAtual->format('s') - $dataPostagem->format('s') . ' segundos atras';
+            $mensagem = $dataAtual->format('s') - $dataPostagem->format('s') . ' segundos atrás';
         }
         return $mensagem;
     }
