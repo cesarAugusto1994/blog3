919800cb5796acd558c8e90d305fccd8aa458dba
Cesar Augusto Sousa <cezzaar@gmail.com>
2016-10-18 16:04:27 -0200

    corrigindo erros no colecoes.js

--- web/assets/blog/js/pages/colecoes.js
+++ web/assets/blog/js/pages/colecoes.js
@@ -6,21 +6,21 @@ function mudarStatus(e) {
 
     e.preventDefault();
 
-    var id = $(this).attr('colecao_id');
+    var id = $(this).attr("colecao_id");
 
-    var situacao = 'Inativar';
+    var situacao = "Inativar";
 
-    if (false == $(this).attr('situacao')) {
-        situacao = 'Ativar';
+    if (false == $(this).attr("situacao")) {
+        situacao = "Ativar";
     }
 
-    alertify.confirm('Deseja ' + situacao + ' esta Cole&ccedil;&atilde;o?', function(){
+    alertify.confirm("Deseja " + situacao + " esta Cole&ccedil;&atilde;o?", function(){
 
         block_screen();
 
         $.ajax({
-            type: 'GET',
-            url: '/admin/colecao/status/' + id,
+            type: "GET",
+            url: "/admin/colecao/status/" + id,
             cache: false,
             success: function (data) {
                 unblock_screen();
@@ -28,10 +28,10 @@ function mudarStatus(e) {
             },
             error: function () {
                 unblock_screen();
-                alertify.error('Ocorreu um erro.');
+                alertify.error("Ocorreu um erro.");
             }
         });
-    }).setting('labels',{'ok':'Sim', 'cancel': 'Cancelar'});
+    }).setting("labels",{"ok":"Sim", "cancel": "Cancelar"});
 };
 
 function editarColecao(e)
@@ -39,9 +39,9 @@ function editarColecao(e)
     e.preventDefault();
 
     $.ajax({
-        type: 'POST',
-        url: '/admin/colecao/save',
-        enctype: 'multipart/form-data',
+        type: "POST",
+        url: "/admin/colecao/save",
+        enctype: "multipart/form-data",
         data: new FormData( this ),
         processData: false,
         contentType: false,
@@ -52,25 +52,25 @@ function editarColecao(e)
         },
         error: function () {
             unblock_screen();
-            alertify.error('Ocorreu um erro.');
+            alertify.error("Ocorreu um erro.");
         }
     });
 
 }
 
-$(document).on('click', '.openMenu', function () {
-    $('.modal-body #id').val($(this).data('id'));
-    $('.modal-body #nome').val($(this).data('nome'));
-    $('.modal-body #descricao').val($(this).data('descricao'));
+$(document).on("click", ".openMenu", function () {
+    $(".modal-body #id").val($(this).data("id"));
+    $(".modal-body #nome").val($(this).data("nome"));
+    $(".modal-body #descricao").val($(this).data("descricao"));
 });
 
-$(document).on('click', '.addColecao', function () {
-    $('.modal-body #id').val('');
-    $('.modal-body #nome').val('');
-    $('.modal-body #descricao').val('');
+$(document).on("click", ".addColecao", function () {
+    $(".modal-body #id").val("");
+    $(".modal-body #nome").val("");
+    $(".modal-body #descricao").val("");
 });
 
 $(document).ready(function () {
-    $('.mudarStatus').on('click', mudarStatus);
-    $('form').submit(editarColecao);
+    $(".mudarStatus").on("click", mudarStatus);
+    $("form").submit(editarColecao);
 });
