872b62c8d2615aabc3073329103cefbb39164a1f
Cesar Augusto Sousa <cezzaar@gmail.com>
2016-10-19 13:49:41 -0200

    correcoes no js

--- web/assets/blog/js/main.js
+++ web/assets/blog/js/main.js
@@ -63,13 +63,11 @@ alertify.defaults = {
 };
 
 function block_screen() {
-
     alertify.message('<p><i class="fa fa-spin fa-spinner"></i> Loading...</p>');
-
     $('<div id="screenBlock"></div>').appendTo('body');
     $('#screenBlock').css( { opacity: 0, background: '#f6f6f6', width: $(document).width(), height: $(document).height() } );
     $('#screenBlock').addClass('blockDiv');
-    $('#screenBlock').animate({opacity: 0.4}, 200);
+    $('#screenBlock').animate({opacity: 0.8}, 200);
 }
 
 function unblock_screen() {
@@ -77,39 +75,3 @@ function unblock_screen() {
         $('#screenBlock').remove();
     });
 }
-
-$(function () {
-    // Remove Search if user Resets Form or hits Escape!
-    $('body, .navbar-collapse form[role="search"] button[type="reset"]').on('click keyup', function(event) {
-        console.log(event.currentTarget);
-        if (event.which == 27 && $('.navbar-collapse form[role="search"]').hasClass('active') ||
-            $(event.currentTarget).attr('type') == 'reset') {
-            closeSearch();
-        }
-    });
-
-    function closeSearch() {
-        var $form = $('.navbar-collapse form[role="search"].active')
-        $form.find('input').val('');
-        $form.removeClass('active');
-    }
-
-    // Show Search if form is not active // event.preventDefault() is important, this prevents the form from submitting
-    $(document).on('click', '.navbar-collapse form[role="search"]:not(.active) button[type="submit"]', function(event) {
-        event.preventDefault();
-        var $form = $(this).closest('form'),
-            $input = $form.find('input');
-        $form.addClass('active');
-        $input.focus();
-
-    });
-    // ONLY FOR DEMO // Please use $('form').submit(function(event)) to track from submission
-    // if your form is ajax remember to call `closeSearch()` to close the search container
-    $(document).on('click', '.navbar-collapse form[role="search"].active button[type="submit"]', function(event) {
-        event.preventDefault();
-        var $form = $(this).closest('form'),
-            $input = $form.find('input');
-        $('#showSearchTerm').text($input.val());
-        closeSearch()
-    });
-});
